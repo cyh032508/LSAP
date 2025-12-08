c581c1b48d80d16a8e7058c024823180fa58ff0e
cyh032508 <chiangian1121@gmail.com>
2025-12-08 17:40:35 +0800

    initial commit

---
+++ hw6/app.js
@@ -0,0 +1,11 @@
+// app.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("<h1>Welcome to the CI/CD Workshop!</h1>");
+});
+
+module.exports = app;
